4045c480e89034db10005b59c4e94509ce01ba14
Tyler Kellen <tyler@sleekcode.net>
2014-01-31 13:52:00 -0500

    add comments

--- bin/hacker.js
+++ bin/hacker.js
@@ -15,6 +15,16 @@ var Hacker = new Liftoff({
 }).on('requireFail', function (name, err) {
   console.log('Unable to load:', name, err);
 }).on('run', function () {
+
+  // console.log('CLI OPTIONS:', this.args);
+  // console.log('CWD:', this.cwd);
+  // console.log('LOCAL MODULES REQUIRED:', this.localRequires);
+  // console.log('EXTENSIONS RECOGNIZED:', this.validExtensions);
+  // console.log('SEARCHING FOR:', this.configNameRegex);
+  // console.log('FOUND CONFIG AT:',  this.configPath);
+  // console.log('CONFIG BASE DIR:', this.configBase);
+  // console.log('YOUR LOCAL MODULE IS LOCATED AT:', this.modulePath);
+
   if(this.configPath) {
     process.chdir(this.configBase);
     require(this.configPath);
