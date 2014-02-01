76147c65a2d78cee42ba83cafede60df15e0bf62
Tyler Kellen <tyler@sleekcode.net>
2014-02-01 15:08:30 -0500

    Update: Improve verbose logging

--- bin/hacker.js
+++ bin/hacker.js
@@ -19,12 +19,14 @@ var Hacker = new Liftoff({
   if(this.args.verbose) {
     console.log('CLI OPTIONS:', this.args);
     console.log('CWD:', this.cwd);
-    console.log('LOCAL MODULES REQUIRED:', this.localRequires);
+    console.log('LOCAL MODULES REQUESTED:', this.localRequires);
     console.log('EXTENSIONS RECOGNIZED:', this.validExtensions);
     console.log('SEARCHING FOR:', this.configNameRegex);
     console.log('FOUND CONFIG AT:',  this.configPath);
     console.log('CONFIG BASE DIR:', this.configBase);
     console.log('YOUR LOCAL DEPS ARE LOCATED:', this.depMap);
+    console.log('LOCAL PACKAGE.JSON:', this.localPackage);
+    console.log('CLI PACKAGE.JSON', require('../package'));
   }
 
   if(this.configPath) {
