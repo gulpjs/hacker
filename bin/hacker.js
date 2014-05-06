0d7b92513385fcbadb7742be818ea3d1606c4d0e
Tyler Kellen <tyler@sleekcode.net>
2014-05-06 10:51:34 -0400

    bump to liftoff 0.10

--- bin/hacker.js
+++ bin/hacker.js
@@ -8,25 +8,21 @@ var Hacker = new Liftoff({
 //  localDeps: ['hacker'],     // these are assigned
 //  configName: 'hackerfile',  // automatically by
 //  processTitle: 'hacker',    // the "name" option
+  extensions: require('interpret').extensions,
   cwdOpt: 'cwd',
   requireOpt: 'require'
 }).on('require', function (name, module) {
-  if (name === 'coffee-script') {
-    module.register();
-  }
+  console.log('Loading:',name);
 }).on('requireFail', function (name, err) {
   console.log('Unable to load:', name, err);
 });
 
-Hacker.launch(launcher);
-
-function launcher (env) {
+var launcher = function (env) {
   if(env.argv.verbose) {
     console.log('LIFTOFF SETTINGS:', this);
     console.log('CLI OPTIONS:', env.argv);
     console.log('CWD:', env.cwd);
     console.log('LOCAL MODULES PRELOADED:', env.preload);
-    console.log('EXTENSIONS RECOGNIZED:', env.validExtensions);
     console.log('SEARCHING FOR:', env.configNameRegex);
     console.log('FOUND CONFIG AT:',  env.configPath);
     console.log('CONFIG BASE DIR:', env.configBase);
@@ -42,3 +38,5 @@ function launcher (env) {
     console.log('No Hackerfile found.');
   }
 }
+
+Hacker.launch(launcher);
