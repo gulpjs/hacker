6c2a00fc244b11423da312874232e53833389fcc
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-06-29 15:50:44 -0700

    use node for windows?

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ describe('hacker', function () {
   this.timeout(5000);
 
   it('can be spawned', function (done) {
-    var output = cp.execSync('../bin/hacker.js', { cwd: __dirname });
+    var output = cp.execSync('node ../bin/hacker.js', { cwd: __dirname });
 
     expect(output.toString()).toEqual('Hackerfile loaded\n');
 
